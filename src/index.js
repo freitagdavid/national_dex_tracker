b1f165b1d4292c7881a1b7cf50c2da101b7f3289
David Freitag <freitagdavid@outlook.com>
2019-12-04 14:39:23 -0500

    index: Using Overmind

--- src/index.js
+++ src/index.js
@@ -1,49 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
-import rootReducer from "./reducers";
-import { persistStore, persistReducer } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
-import storage from "redux-persist/lib/storage";
-import hardSet from "redux-persist/lib/stateReconciler/hardSet";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
-const persistConfig = {
-    key: "root",
-    storage,
-    stateReconciler: hardSet
-};
-
-function selectMiddleware() {
-    const middleList = [thunk];
-    if (process.env.NODE_ENV === "development") {
-        middleList.push(logger);
-    }
-    return middleList;
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-    persistedReducer,
-    composeEnhancers(applyMiddleware(...selectMiddleware()))
-);
-let persistor = persistStore(store);
+import { Provider as OMProvider } from 'overmind-react'
+import { config } from "./app/"
+import { createOvermind } from "overmind";
 
 // const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
+export const overmind = createOvermind(config, {
+    devtools: true
+})
+
 ReactDOM.render(
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <App />
-        </PersistGate>
-    </Provider>,
+    <OMProvider value={ overmind }>
+        <App />
+    </OMProvider>,
     document.getElementById("root")
 );
 
