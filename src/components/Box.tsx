f1734f43a8e9653edfaf22cc597c3216ddce6259
David Freitag <freitagdavid@outlook.com>
2024-02-04 09:01:34 -0500

    Add some lazy loading

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -4,28 +4,30 @@ import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "./ui/collap
 import { Progress } from "./ui/progress";
 import { useAtom } from "jotai";
 import { boxCaughtAtom } from "@/state";
+import { useState } from "react";
 
-export const Box = ({box, boxNum}: {box: Pokemon_V2_Pokemonspecies[]; boxNum: number}) => {
+export const Box = ({ box, boxNum }: { box: Pokemon_V2_Pokemonspecies[]; boxNum: number }) => {
 
-    const [caught] = useAtom(boxCaughtAtom(boxNum));
-    const caughtPercent = (caught / 30) * 100;
+  const [caught] = useAtom(boxCaughtAtom(boxNum));
+  const caughtPercent = (caught / 30) * 100;
+  const [isOpen, setIsOpen] = useState(true);
 
 
-    return (
-        <Collapsible className="w-11/12 shadow-md border-border border-solid border-2 mb-8 max-w-304">
-                <CollapsibleTrigger className="w-full bg-accent text-primary text-center h-14 pb-0">
-                  <div className="h-10 flex w-full justify-center flex-col">Box {boxNum + 1}</div>
-                  <div className="h-4 w-full m-0 p-0">
-                    <Progress value={caughtPercent} className="m-0 p-0 rounded-none bg-red-600" fillClassName="bg-green-500" />
-                  </div>
-                </CollapsibleTrigger>
-                <CollapsibleContent className="flex flex-wrap justify-evenly">
-                  {box.map((poke) => {
-                    return (
-                      <PokemonCard poke={poke} key={poke.id} boxNum={boxNum}/>
-                    )
-                  })}
-                </CollapsibleContent>
-              </Collapsible>
-    )
+  return (
+    <Collapsible className="w-11/12 shadow-md border-border border-solid border-2 mb-8 max-w-304" open={isOpen} onOpenChange={setIsOpen}>
+      <CollapsibleTrigger className="w-full bg-accent text-primary text-center h-14 pb-0">
+        <div className="h-10 flex w-full justify-center flex-col">Box {boxNum + 1}</div>
+        <div className="h-4 w-full m-0 p-0">
+          <Progress value={caughtPercent} className="m-0 p-0 rounded-none bg-red-600" fillClassName="bg-green-500" />
+        </div>
+      </CollapsibleTrigger>
+      <CollapsibleContent className="flex flex-wrap justify-evenly">
+        {box.map((poke) => {
+          return (
+            <PokemonCard poke={poke} key={poke.id} boxNum={boxNum} />
+          )
+        })}
+      </CollapsibleContent>
+    </Collapsible>
+  )
 };
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -4,12 +4,37 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card"
 import { caughtStatus } from "@/state";
 import { useAtom } from "jotai";
 import { cn } from "@/lib/utils";
+import { Skeleton } from "./ui/skeleton";
+import { useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+export const SkeletonCard = () => {
+    return (
+        < Card className="aspect-square flex flex-col align-middle justify-between w-44 mt-3">
+            <CardHeader className='py-2 border-b-[1px] border-border border-solid'>
+                <Skeleton className='w-full text-center h-6' />
+            </CardHeader>
+            <CardContent className='px-0 pb-0 flex justify-center'>
+                <Skeleton className="w-[100px] h-[100px]" />
+            </CardContent>
+            <CardFooter className='w-full flex justify-center pb-0 px-0'>
+                <Skeleton className='w-full' />
+            </CardFooter>
+        </Card >
+    )
+}
 
 export const PokemonCard = ({ poke, boxNum }: { poke: Pokemon_V2_Pokemonspecies, key: number, boxNum: number }) => {
     const [caught, setCaught] = useAtom(caughtStatus(poke.id));
 
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    const handleLoad = () => {
+        setIsLoaded(true);
+    }
+
     const handleCaught = () => {
-        setCaught({caught: !caught, boxNum: boxNum})
+        setCaught({ caught: !caught, boxNum: boxNum })
     }
 
     return (
@@ -19,7 +44,15 @@ export const PokemonCard = ({ poke, boxNum }: { poke: Pokemon_V2_Pokemonspecies,
                     + poke.name.slice(1)}</CardTitle>
             </CardHeader>
             <CardContent className='px-0 pb-0 flex justify-center'>
-                <img src={poke.pokemon_v2_pokemons[0].pokemon_v2_pokemonsprites[0].sprites.front_default} alt={poke.name} className="w-[100px] h-[100px]" />
+                <LazyLoadImage
+                    src={poke.pokemon_v2_pokemons[0].pokemon_v2_pokemonsprites[0].sprites.front_default}
+                    alt={poke.name}
+                    className="w-[100px] h-[100px]"
+                    width={100}
+                    height={100}
+                    onLoad={handleLoad}
+                    placeholder={<Skeleton className="w-[100px] h-[100px]" />}
+                />
             </CardContent>
             <CardFooter className='w-full flex justify-center pb-0 px-0'>
                 <Button className={cn('w-full', caught ? 'bg-green-500 hover:bg-green-700' : 'bg-red-500 hover:bg-red-700')}>
