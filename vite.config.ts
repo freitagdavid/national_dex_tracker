4f6598fbf0f64c50890c1b3ad27e855f17c716aa
David Freitag <freitagdavid@outlook.com>
2024-02-06 09:43:44 -0500

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/national_dex_tracker/",
+    base: "./",
     plugins: [
         react({
             babel: {
