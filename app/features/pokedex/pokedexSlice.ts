cf9f5319092e9ab9da3030f003db3ba1e5f7d75e
David Freitag <freitagdavid@outlook.com>
2024-11-13 23:25:51 -0500

    Update linting and formatting to biome

--- app/features/pokedex/pokedexSlice.ts
+++ app/features/pokedex/pokedexSlice.ts
@@ -1,30 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
 
 type caughtEntry = {
-    id: number;
-    name: string;
-    caught: boolean;
-}
+  id: number;
+  name: string;
+  caught: boolean;
+};
 
 export interface PokedexState {
-    caught: caughtEntry[];
+  caught: caughtEntry[];
 }
 
 const initialState: PokedexState = {
-    caught: []
-}
-
+  caught: [],
+};
 
 export const pokedexSlice = createSlice({
-    name: 'pokedex',
-    initialState,
-    reducers: {
-        toggleCaughtStatus: (state, action: PayloadAction<number>) => {
-            state.caught.map((item, index) => {
-                if (item.id === action.payload) {
-                    state.caught[index].caught = !state.caught[index].caught;
-                }
-            })
-    }
+  name: 'pokedex',
+  initialState,
+  reducers: {
+    toggleCaughtStatus: (state, action: PayloadAction<number>) => {
+      state.caught.map((item, index) => {
+        if (item.id === action.payload) {
+          state.caught[index].caught = !state.caught[index].caught;
+        }
+      });
+    },
+  },
 });
